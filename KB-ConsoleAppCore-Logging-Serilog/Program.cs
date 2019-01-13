c47d345df3f711d64f9d8b268b4a4d99b3ddb664
Zajic Pavel <zajic@frisoft.cz>
2019-01-13 19:30:28 +0100

    Add project files.

---
+++ KB-ConsoleAppCore-Logging-Serilog/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KB_ConsoleAppCore_Logging_Serilog
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
